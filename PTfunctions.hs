9b6df3ffc640c74421dff3db8a55b68a4f67fe87
thKyomno18 <thkyomno18@hanovernorwichschools.org>
2019-02-04 15:16:28 +0000

    PTfuntions.hs take #1

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -3,9 +3,9 @@ quadraticVertex a b c = (0-b)/(2*a)
 quadraticDiscriminant a b c = (b**2)-(4*a*c)
 quadraticRealSolutionQty a b c = if (quadraticDiscriminant a b c)>=0
     then if (quadraticDiscriminant a b c)>0
-        then putStrLn "2 solutions"
-        else putStrLn "1 solution"
-else putStrLn "0 solutions"
+        then 2
+        else 1
+else 0
 convertFtoC f = (f-32)/1.8
 convertCtoF c = (1.8*c)+32
 
